ffed1931163903cb5cf0f79820e57bfd7e3c84d4
kth1017 <96664524+kth1017@users.noreply.github.com>
2023-02-19 21:56:25 +0900

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
-import React from "react";
-import "./style.css";
+import React, { useEffect, useState, useContext } from 'react';
+import axios from 'axios';
+import {
+  Container,
+  Grid,
+  Button,
+  ButtonGroup,
+  Input,
+  TextField,
+  Typography,
+} from '@mui/material';
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@mui/material';
+import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
+
+function Box(){
+  return <Container sx={{ border: 1, padding:2, borderColor: 'divider' }}>
+  <Grid Container>
+      <Grid item xs={2}>   
+      질문 입력<br/>    
+      <FormProvider><Form></Form></FormProvider>
+      </Grid>
+      <Grid item xs={10}> 
+      추천 질문<br/><br/> 
+      <ButtonGroup variant="outlined" aria-label="outlined button group">
+      <ButtonForm></ButtonForm>
+      </ButtonGroup></Grid>  
+  </Grid>
+  </Container>
+}
+
+function Form(){
+
+}
+
+function ButtonForm(){
+  
+}
+
 
 export default function App() {
   return (

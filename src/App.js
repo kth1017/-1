e3a9741c9bf48f01cffabc6d1f32df00a98c1b50
fadet <96664524+kth1017@users.noreply.github.com>
2023-02-20 01:18:25 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,55 +19,73 @@ import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
 
 const ModContext = React.createContext();
 function ModProvider({ children }) {
-  const modState = useState('state1');
+  const a = ['wsqs', 'stcxxxxxcate2'];
+  const modState = useState(a);
   return <ModContext.Provider value={modState}>{children}</ModContext.Provider>;
 }
 
-function useModState() {
+function useModState(putIndex) {
   const value = useContext(ModContext);
   const a = ModContext;
   if (value === undefined) {
     throw new Error('error');
   }
-  return value;
+  const index = putIndex;
+  return [ value[0][index],
+    (newValue) => {
+      const newArray = [...value[0]];
+      newArray[index] = newValue;
+      value[1](newArray);
+    },
+  ];
 }
+
+
+
 
 function Box(props) {
   const [bindingQ, setBindingQ] = useModState();
+
   return (
-    <Container sx={{ border: 1, padding: 2, borderColor: 'divider' }}>
-      <Grid Container>
-        <Grid item xs="2">
-          질문 입력
-          <br />
-          <Form
-            onForm={(_originQ) => {
-              setBindingQ(_originQ);
-            }}
-          ></Form>
+    <>
+      <Container sx={{ border: 1, padding: 2, borderColor: 'divider' }}>
+        <Grid Container>
+          <Grid item xs="2">
+            질문 입력
+            <br />
+            <Form
+              onForm={(_originQ) => {
+                setBindingQ(_originQ);
+              }}
+            ></Form>
+          </Grid>
+          <Grid item xs="10">
+            추천 질문
+            <br />
+            <br />
+            <ButtonGroup variant="outlined" aria-label="outlined button group">
+              <ButtonForm></ButtonForm>
+            </ButtonGroup>
+          </Grid>
         </Grid>
-        <Grid item xs="10">
-          추천 질문
-          <br />
-          <br />
-          <ButtonGroup variant="outlined" aria-label="outlined button group">
-            <ButtonForm></ButtonForm>
-          </ButtonGroup>
-        </Grid>
-      </Grid>
-      <div>{bindingQ}</div>
-    </Container>
+      </Container>
+      <Container>
+        <TransForm></TransForm>
+      </Container>
+    </>
   );
 }
 
 function Form(props) {
-  const [bindingQ, setBindingQ] = useModState();
+  const [bindingQ, setBindingQ] = useModState(0);
+  const [transQ, setTransQ] = useModState();
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         const originQ = event.target.originQ.value;
         props.onForm(originQ);
+        setTransQ('번역');
       }}
     >
       <p>
@@ -91,7 +109,8 @@ function Form(props) {
 }
 
 function ButtonForm(props) {
-  const [bindingQ, setBindingQ] = useModState();
+  const [bindingQ, setBindingQ] = useModState(0);
+  const [transQ, setTransQ] = useModState(1);
   const qArr = ['java', 'spring', 'js'];
   const grouping = () => {
     const result = [];
@@ -115,11 +134,44 @@ function ButtonForm(props) {
   return grouping();
 }
 
+function TransForm() {
+  const [transQ, setTransQ] = useModState();
+  return (
+    <form
+      onSubmit={(event) => {
+        const LocalTransQ = event.target.transQ.value;
+        event.preventDefault();
+        setTransQ(LocalTransQ);
+        console.log(transQ, 'transQ');
+      }}
+    >
+      <p>
+        <Input
+          required
+          type="text"
+          name="transQ"
+          placeholder="영어로 직접 입력 가능"
+          value={transQ || ''}
+          onChange={(event) => {
+            setTransQ(event.target.value);
+          }}
+        />
+      </p>
+      <p>
+        <Button variant="outlined" type="submit">
+          ai에게 질문
+        </Button>
+      </p>
+    </form>
+  );
+}
+
 export default function App() {
   return (
     <div>
       <ModProvider>
         <Box></Box>
+        
       </ModProvider>
     </div>
   );
